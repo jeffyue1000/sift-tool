8c58e169a1ccdaa9075fd874360ba1bc8b35ec5f
Rohan Agarwal <rohanagarwal@rohans-mbp.myfiosgateway.com>
2024-06-22 04:00:00 -0400

    feat: finished prelim login screen + navbar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             <div className="content-container">
                 <Routes>
                     <Route
-                        path="/"
+                        path="/login"
                         element={<LoginScreen />}
                     />
                     <Route
