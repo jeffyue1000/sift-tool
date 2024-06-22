bd609336d6370eaa8e5fb126fb297f0f207b4417
jeffyue1000 <jeffyue1000@gmail.com>
2024-06-22 04:06:20 -0400

    fixed login routing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route
-                    path="/"
+                    path="/login"
                     element={<LoginScreen />}
                 />
                 <Route
