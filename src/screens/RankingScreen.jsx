37010cd4734834d2a863d7de2c5b8fb9438e7b2d
Rohan Agarwal <rohanagarwal@rohans-mbp.myfiosgateway.com>
2024-07-02 20:25:15 -0400

    committing RankingScreen

--- src/screens/RankingScreen.jsx
+++ src/screens/RankingScreen.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ApplicantRank from "../components/ApplicantRank";
+import "../styles/RankingScreen.css";
 
 export default function RankingScreen() {
     const [applicants, setApplicants] = useState([]);
 
     const fetchApplicants = async () => {
         try {
-            const response = await axios.get(`http://localhost:3001/resumes/getResumes`);
+            const response = await axios.get(
+                `http://localhost:3001/resumes/getResumes`
+            );
             console.log(response.data);
             setApplicants(response.data);
         } catch (error) {
@@ -20,16 +23,19 @@ export default function RankingScreen() {
     }, []);
 
     return (
-        <div className="ranking-screen">
-            {applicants.map((applicant) => (
-                <ApplicantRank
-                    key={applicant._id}
-                    name={applicant.name}
-                    gradYear={applicant.gradYear}
-                    eloScore={applicant.eloScore}
-                    rank={applicant.rank}
-                />
-            ))}
+        <div>
+            <h1 className="header">Aggregate Rankings</h1>
+            <div className="ranking-screen">
+                {applicants.map((applicant) => (
+                    <ApplicantRank
+                        key={applicant._id}
+                        name={applicant.name}
+                        gradYear={applicant.gradYear}
+                        eloScore={applicant.eloScore}
+                        rank={applicant.rank}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
