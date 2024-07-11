3ef1d35b063d45eba3d3ceb6b225470597cc6c87
Rohan Agarwal <rohanagarwal@rohans-mbp.myfiosgateway.com>
2024-07-11 01:19:04 -0400

    welcome screen and login screen styling down

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/WelcomeScreen.css";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function WelcomeScreen() {
     return (
@@ -7,39 +8,67 @@ export default function WelcomeScreen() {
             <div className="welcome-content">
                 {/* <h2 className="welcome-description">Introducing</h2> */}
                 <h1 className="welcome-title">SIFT</h1>
-                <h2 className="welcome-subtitle">Collaborative Resume Review and Ranking Tool</h2>
+                <h2 className="welcome-subtitle">
+                    Collaborative Resume Review and Ranking Tool
+                </h2>
                 <p className="welcome-instructions"></p>
-
-                <p className="welcome-instructions">How to get started:</p>
-                <ol className="instruction-list">
-                    <li className="welcome-instruction-item">Create your group's unique session key & password</li>
-                    <li className="welcome-instruction-item">
-                        Complete the payment process through Stripe to activate your session
-                    </li>
-                    <li className="welcome-instruction-item">
-                        Share the session key with your group and you are ready to start sifting!
-                    </li>
-                </ol>
-                <p className="sift-instructions">Page-by-Page Instructions:</p>
-                <ol className="instruction-list">
-                    <li className="sift-instruction-item">
-                        Session Login: Enter your session key to join your group's sifting session.
-                    </li>
-                    <li className="sift-instruction-item">
-                        Upload Resumes: Upload resumes with the following naming convention:
-                        FirstName_LastName_Resume_GradYear.pdf. Once you upload a resume, you will not be able to remove
-                        it from your session.{" "}
-                    </li>
-                    <li className="sift-instruction-item">
-                        Compare Resumes: You will be presented with two resumes at a time. Click on the resume you
-                        believe is stronger. This will bring up the next pair of resumes. Repeat this process.{" "}
-                    </li>
-                    <li className="sift-instruction-item">
-                        Resume Rankings: Here you will be able to view your session's aggregated resume rankings. The
-                        more comparisons you make, the more accurate the overall ranking will be.{" "}
-                    </li>
-                </ol>
-                <p className="faq-header">Frequently Asked Questions:</p>
+                <div className="instructions-wrapper">
+                    {/* <div className="welcome-instructions-container">
+                        <p className="welcome-instructions">
+                            How to get started:
+                        </p>
+                        <ol className="instruction-list">
+                            <li className="welcome-instruction-item">
+                                Create your group's unique session key &
+                                password
+                            </li>
+                            <li className="welcome-instruction-item">
+                                Complete the payment process through Stripe to
+                                activate your session
+                            </li>
+                            <li className="welcome-instruction-item">
+                                Share the session key with your group and start
+                                sifting!
+                            </li>
+                        </ol>
+                    </div> */}
+                    <div className="sift-instructions-container">
+                        <p className="sift-instructions">
+                            Sifting Instructions
+                        </p>
+                        <ol className="instruction-list">
+                            <li className="sift-instruction-item">
+                                Login: Enter your session key to join your
+                                group's sifting session or create your group's
+                                unique session
+                            </li>
+                            <li className="sift-instruction-item">
+                                Upload: Upload resumes in PDF format. Once you
+                                upload a resume, you cannot remove it from your
+                                session.{" "}
+                            </li>
+                            <li className="sift-instruction-item">
+                                Compare: You will be presented with two resumes
+                                at a time. Click on the resume you believe is
+                                stronger. Repeat this process.{" "}
+                            </li>
+                            <li className="sift-instruction-item">
+                                Rankings: View your group resume rankings. The
+                                more comparisons you make, the more accurate the
+                                overall ranking will be.{" "}
+                            </li>
+                        </ol>
+                        <Link
+                            to="/login"
+                            className="start-button-link"
+                        >
+                            <button className="start-button">
+                                Get Started
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+                {/* <p className="faq-header">Frequently Asked Questions:</p>
                 <ul className="faq-list">
                     <li className="faq-item">
                         <h3 className="faq-question">placeholder question</h3>
@@ -57,7 +86,7 @@ export default function WelcomeScreen() {
                         <h3 className="faq-question">placeholder question</h3>
                         <p className="faq-answer">placeholder answer</p>
                     </li>
-                </ul>
+                </ul> */}
             </div>
             <footer>© 2024 Sift. All rights reserved.</footer>
         </div>
